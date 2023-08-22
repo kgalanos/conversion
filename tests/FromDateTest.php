b8923d1391021dfbf42e32b19058ab832222e2e7
Kostas Galanos <kigalanos@gmail.com>
2023-08-22 15:01:48 +0300

    some updates

--- tests/FromDateTest.php
+++ tests/FromDateTest.php
@@ -5,8 +5,8 @@ use Carbon\Carbon;
 
 it('can test', function () {
     $value = Carbon::create(2023, 2, 1);
-    $tmp = \kgalanos\conversion\stringFromDate::value($value)->toString();
+    $tmp = \kgalanos\conversion\Date\ToString::setValue($value)->format();
     expect('2023-02-01')->toEqual($tmp);
-    $tmp = \kgalanos\conversion\stringFromDate::value($value, 'd-m-Y')->toString();
+    $tmp = \kgalanos\conversion\Date\ToString::setValue($value, 'd-m-Y')->format();
     expect('01-02-2023')->toEqual($tmp);
 });
